26fc0adeffac1d261187e6d870a99854271fe004
hj <hujiao24@163.com>
2025-10-25 09:08:13 +0800

    fix url

--- .vscode/update_domain.py
+++ .vscode/update_domain.py
@@ -1,7 +1,7 @@
 import re
 import sys
 
-DOMAIN = "https://hujiao24.github.io"
+DOMAIN = "https://blog.qc7.org/"
 
 def process_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
